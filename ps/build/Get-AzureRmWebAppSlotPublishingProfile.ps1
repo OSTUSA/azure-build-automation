d2f057bef46923f0d20a66ee6c5127b6378df539
Dan Tape <dtape@ostusa.com>
2017-11-14 20:38:09 -0500

    simplify build scripts

--- ps/build/Get-AzureRmWebAppSlotPublishingProfile.ps1
+++ ps/build/Get-AzureRmWebAppSlotPublishingProfile.ps1
@@ -3,8 +3,8 @@ param (
 	[Parameter(Mandatory=$true)][string]$applicationId,
     [Parameter(Mandatory=$true)][string]$applicationKey,
     [Parameter(Mandatory=$true)][string]$resourceGroup,
-    [Parameter(Mandatory=$true)][string[]]$applications,
-    [Parameter(Mandatory=$true)][string]$slotName
+    [Parameter(Mandatory=$true)][string]$application,
+    [Parameter(Mandatory=$true)][string]$slot
 )
 
 trap
@@ -19,7 +19,4 @@ $SecurePassword = $applicationKey | ConvertTo-SecureString -AsPlainText -Force
 $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $applicationId, $SecurePassword
 Add-AzureRmAccount -Credential $cred -Tenant $tenantId -ServicePrincipal
 
-foreach($application in $applications)
-{
-    Get-AzureRmWebAppSlotPublishingProfile -ResourceGroupName $resourceGroup -Name $application -Slot $slotName -Format WebDeploy -OutputFile "$application.publishsettings"
-}
+Get-AzureRmWebAppSlotPublishingProfile -ResourceGroupName $resourceGroup -Name $application -Slot $slot -Format WebDeploy -OutputFile "$application.publishsettings"
